40b57405c17626c3558e407d5719bc7d66b47299
XTZhu <a1291753307@qq.com>
2021-07-10 19:33:55 +0800

    feat: log middleware

---
+++ apps/nest-demo/src/logger.middleware.ts
@@ -0,0 +1,17 @@
+import { Injectable, Logger, NestMiddleware } from "@nestjs/common";
+import { NextFunction } from "express";
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction): void {
+    // console.log('Requesting...');
+    Logger.log('Requesting...')
+    next();
+  }
+}
+
+
+export function logger(req: Request, res: Response, next: NextFunction): void {
+  console.log('function Requesting...');
+  next();
+}
